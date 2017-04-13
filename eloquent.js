5af4ef1ed25b3fcd5418a16f8e6c101e7e7d9b05
ElenaSchubel <ebassist@live.com>
2017-04-13 16:33:21 +1200

    finished problems 2 and 3

--- eloquent.js
+++ eloquent.js
@@ -4,10 +4,53 @@ function min(a, b) {
       return a;
     else
       return b;
-    
+
 }
 
 console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
+
+
+
+// second problem
+function isEven(num) {
+  if (num == 0)
+  return true;
+  else if (num ==1)
+  return false;
+  else if (num < 0)
+   return isEven(-num);
+  else
+   return isEven(num - 2);
+}
+
+
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
+
+
+//problem 3
+
+function countChar(str, ch) {
+  var count = 0;
+  for (var i = 0; i < str.length; i++)
+    if (str.charAt(i) == ch)
+      count += 1;
+  return count;
+}
+
+function countBs(str) {
+  return countChar(str, "B");
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
